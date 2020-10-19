48b9c9adf7d040d1f0ac69b03043acf240362936
Cruzer-S <yyyynoom@gmail.com>
2020-10-19 11:07:06 +0900

    Chapter 15. Exercise

---
+++ Chapter 15/test_ex4/debug.h
@@ -0,0 +1,5 @@
+#ifdef DEBUG
+#define PRINT_DEBUG(n) printf("Value of " #n ": %d\n", n)
+#else
+#define PRINT_DEBUG(n)
+#endif
---
+++ Chapter 15/test_ex4/testdebug.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+#include "debug.h"
+#define DEBUG
+
+int main(void)
+{
+	int i = 1, j = 2, k = 3;
+
+#ifdef DEBUG
+	printf("Output if DEBUG is defined: \n");
+#else
+	printf("Output if DEBIG is not defined: \n");
+#endif
+
+	PRINT_DEBUG(i);
+	PRINT_DEBUG(j);
+	PRINT_DEBUG(k);
+	PRINT_DEBUG(i + j);
+	PRINT_DEBUG(2 * i + j - k);
+
+	return 0;
+}
